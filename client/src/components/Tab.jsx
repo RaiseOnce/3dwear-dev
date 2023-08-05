61e1b7de79261913987a6c861055da50c1f1a20a
Otto Rahn <elias.yunusoff@gmail.com>
2023-08-05 17:55:59 +0500

    customizer is pre-done

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import { useSnapshot } from 'valtio'
 
-const Tab = () => {
-  return <div>Tab</div>
+import state from '../store'
+
+const Tab = ({ tab, isFilterTab, isActiveTab, handleClick }) => {
+  const snap = useSnapshot(state)
+
+  const acitveStyles =
+    isFilterTab && isActiveTab
+      ? { backgroundColor: snap.color, opacity: 0.5 }
+      : { backgroundColor: 'transperent', opacity: 1 }
+
+  return (
+    <div
+      key={tab.name}
+      className={`tab-btn ${
+        isFilterTab ? 'rounded-full glassmorhism' : 'rounded-4'
+      }`}
+      onClick={handleClick}
+      style={acitveStyles}
+    >
+      <img
+        src={tab.icon}
+        alt={tab.name}
+        className={`${
+          isFilterTab ? 'w-2/3 h-2/3' : 'w-11/12 h-11/12 object-contain'
+        }`}
+      />
+    </div>
+  )
 }
 
 export default Tab
